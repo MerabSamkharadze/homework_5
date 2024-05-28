91521b63e79d2aeaeb1c2dc58ec5fb141079cc6a
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-05-28 22:32:02 +0400

    task 5 is done!

--- script.js
+++ script.js
@@ -27,3 +27,15 @@ userAnswer.toLowerCase() === corectAnswer
 let array5 = [12, 25, 3, 6, 8, 14, 7, 23];
 let array5New = array5.map((item) => item / 3);
 console.log(array5New);
+
+// task 4
+
+let array1 = ["hello1", 14, 24, "hello2"];
+let array1New = array1.filter((item) => typeof item === "number");
+console.log(array1New);
+
+// task 5
+
+let languages = ["html", "css", "javascript", "python", "php"];
+let languagesNew = languages.filter((item) => item.length > 3);
+console.log(languagesNew);
